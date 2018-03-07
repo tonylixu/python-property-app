85ea302a8f1691336e788558e95d1a647dcb00b7
Tony Li Xu <tonylixu@gmail.com>
2018-03-06 22:27:20 -0500

    Added comments

--- Apartment.py
+++ Apartment.py
@@ -1,3 +1,6 @@
+"""
+Apartment class
+"""
 from .Property import Property
 
 Class Apartment(Property):
@@ -5,30 +8,36 @@ Class Apartment(Property):
     valid_balconies = {"yes", "no", "solarium"}
     
     def __init__(self, balcony='', laundry='', **kwargs):
+        # Make sure Property class is properly initialized
         super().__init__(**kwargs)
         self.balcony = balcony
         self.laundry = laundry
         
     def display(self):
+        # Make sure display method in Property is properly initialized
         super().display()
         print("APARTMENT DETAIS")
         print("Laundry: {}".format(self.laundry))
         print("has balcony: {}".format(self.balcony))
         
         parent_init = Property.prompt_init()
+        # Adding laundry
         laundry = ''
         while laundry.lower() not in Apartment.valid_laundries:
             laundry = input(
                 "What laundry facilities does "
                 "the propety have? ({})".format(
                 ", ".join(Apartment.valid_laundries)))
-            
+        
+        # Adding balcony
         balcony = ''
         while balcony.lower() not in Apartment.valid_balconies:
             balcony = input(
                 "Does the property have a balcony? "
                 "({})".format(
                 ", ".join(Apartment.valid_balconies)))
+        
+        # Dict update method to merge new dict values
         parent_init.update({
             "laundry": laundry,
             "balcony": balcony
