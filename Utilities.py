7480a260aebd24ddf48a9767e1239c74f74fb7e9
Tony Li Xu <tonylixu@gmail.com>
2018-03-06 22:40:40 -0500

    Added Utilities file

---
+++ Utilities.py
@@ -0,0 +1,16 @@
+"""
+Utilities class
+
+Contains all common functions
+"""
+
+Class Utilities:
+    def __init__(self):
+        pass
+    
+    def get_valid_input(input_string, valid_options):
+        input_string += " ({})".format(", ".join(valid_options))
+        response = input(input_string)
+        while response.lower() not in valid_options:
+            response = input(input_string)
+        return response
