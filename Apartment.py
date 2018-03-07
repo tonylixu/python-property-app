5b6e6d4f93a87988ff58eab9ca827a7668b6d278
Tony Li Xu <tonylixu@gmail.com>
2018-03-06 22:23:08 -0500

    Added display method

--- Apartment.py
+++ Apartment.py
@@ -14,3 +14,25 @@ Class Apartment(Property):
         print("APARTMENT DETAIS")
         print("Laundry: {}".format(self.laundry))
         print("has balcony: {}".format(self.balcony))
+        
+        parent_init = Property.prompt_init()
+        laundry = ''
+        while laundry.lower() not in Apartment.valid_laundries:
+            laundry = input(
+                "What laundry facilities does "
+                "the propety have? ({})".format(
+                ", ".join(Apartment.valid_laundries)))
+            
+        balcony = ''
+        while balcony.lower() not in Apartment.valid_balconies:
+            balcony = input(
+                "Does the property have a balcony? "
+                "({})".format(
+                ", ".join(Apartment.valid_balconies)))
+        parent_init.update({
+            "laundry": laundry,
+            "balcony": balcony
+        })
+        
+        return parent_init
+    prompt_init = staticmethod(prompt_init)
