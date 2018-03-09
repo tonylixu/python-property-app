d09f0b5a003691c2353ee7202ea584bcbe06d29f
tonylixu@gmail.com <tonylixu@gmail.com>
2018-03-08 21:30:01 -0500

    Added __init__ method

--- House.py
+++ House.py
@@ -6,3 +6,10 @@ from .Property import Property
 class House(Property):
     valid_garage = {"attached", "detached", "none"}
     valid_fenced = {"yes", "no"}
+
+    def __init__(self, num_stories='', garage='', fenced='', **kwargs):
+        # Make sure Property class is properly initialized
+        super().__init__(**kwargs)
+        self.garage = balcony
+        self.fenced = laundry
+        self.num_stories = num_stories
